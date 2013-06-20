7e868e7bc53c4b0e2d5bd9f9cbdc3488c98e2ed6
unknown <justinsmith@GX7800010.sd54.k12.il.us>
2013-06-20 07:43:53 -0500

    updated with reddit user suggestion

--- dailyprog130easy.py
+++ dailyprog130easy.py
@@ -4,13 +4,10 @@ import sys
 die = sys.argv[1]
 
 attr = die.split('d')
-
-rolls = attr[0]
-sides = attr[1]
+rolls, sides = sys.argv[1].split('d')
 
 result = []
 
-for x in range(int(rolls)):
-	output.append(random.randint(2,int(sides)))
+result = ' '.join([str(random.randrange(1, int(sides)+1)) for die in range(int(rolls))])
 
 print result
